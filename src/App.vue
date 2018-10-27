c8093861e0540ea60a778d505a868526b158d734
Christopher Connock <christopher.connock@gmail.com>
2018-10-27 14:15:44 -0400

    match prexisting setup

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,55 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app>
+    <v-navigation-drawer 
+      v-model="drawer"
+      app
+    >
+      <v-list dense subheader>
+          <v-subheader>The main stuff</v-subheader>
+          <v-list-tile>
+            <router-link to="/">Home</router-link>
+          </v-list-tile>
+      </v-list>
+      <v-list dense subheader>
+          <v-subheader>AR.js Examples</v-subheader>
+          <v-list-tile>
+            <router-link to="/Basic">Basic</router-link>
+          </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-toolbar app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>BLUR</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">SOURCE</span>
+      </v-btn>
+    </v-toolbar>
+
+    <v-content>
+      <router-view/>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  data () {
+    return {
+      drawer: false
     }
   }
 }
-</style>
+</script>
