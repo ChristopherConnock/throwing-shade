fbb4da2e139655fc0649c64af1605857f8000dd6
Christopher Connock <christopher.connock@gmail.com>
2018-10-27 16:30:21 -0400

    spheres

---
+++ src/components/VueBox.vue
@@ -0,0 +1,13 @@
+<template>
+  <a-sphere position="0 {{y}} -5" radius="1.25" color="#EF2D5E"></a-sphere>
+</template>
+
+<script>
+export default {
+    name: 'vue-box',
+        props: {
+        location: Number
+        }
+    }
+}
+</script>
